258c4fe689632eea50833660259f1b7d7c07eeeb
Akhil <akhilk1904@gmail.com>
2026-04-28 22:10:13 +0530

    fix2

--- app.js
+++ app.js
@@ -44,4 +44,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(3000, '0.0.0.0', () => {
+  console.log("Server running on port 3000");
+});
+
 module.exports = app;
